91ac801f0b746340589d320bab9193ae33500552
Rubel18203 <mdrubelbabu824@gmail.com>
2024-12-04 02:28:47 +0600

    second update

--- blog/resources/views/Admin/Category.blade.php
+++ blog/resources/views/Admin/Category.blade.php
@@ -3,6 +3,18 @@
    <head>
        <!-- Required meta tags -->
      @include('Admin.Css')
+     <style>
+      .div{
+        padding-top:100px;
+      }
+       .h2{
+         font-size:40px;
+         padding-bottom:100px;
+       }
+       .form{
+        padding-bottom:1000px;
+       }
+     </style>
     </head>
   <body>
     <div class="container-scroller">
@@ -12,11 +24,20 @@
         <!-- partial:partials/_navbar.html -->
          @include('Admin.header')
         <!-- partial -->
-        <div class="main-panel">
-        <div class="content-wrapper">
+         <div class="main-panel">
+           <div class="div">
             
+                <form>
+                <h2>Add Category</h2>
+                  <input type="text" name="username" placeholder="write category name">
+                  <input type="submit"  class="btn btn primary" value="Submit"  value="Add category">
+
+
+                </form>
+              
+            </div>
           
-        
+          </div>
 
         
     <!-- container-scroller -->
